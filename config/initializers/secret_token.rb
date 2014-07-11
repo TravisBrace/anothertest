a98d3f7b9dc6abb7daef0b0b0199733c844dafab
Travis Brace <travisbrace@gmail.com>
2014-07-10 20:14:37 -0400

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Againtesting::Application.config.secret_token = '131419d7b359580792042d9cf175823cd703ba08a4b919f88cd815058e3057385b66f6d2d7b3a453080e77c649d854bad3dae474df32fe03e8d5cfc16fb64cd2'
