f44acdbe47f0fa4438c02a380d7bb437bf589a9e
travisbrace <travisbrace.com>
2014-07-12 15:00:45 -0400

    updated precompile #

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module Againtesting
     config.assets.version = '1.0'
 
     #yuuhhppp
-    config.assets.initialize_on_precompile = true
+    #config.assets.initialize_on_precompile = true
 
   end
 end
